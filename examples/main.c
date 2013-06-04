513474d4c2c70a010fc13da1ff9025a0d772b65e
Øyvind Kolås <pippin@gimp.org>
2013-06-04 21:46:40 +0200

    fix main example

--- examples/main.c
+++ examples/main.c
@@ -15,14 +15,15 @@ static void each_arg_cb (Var *argb, void *oi)
 {
   Var *test = var_new(NULL,NULL);
 
-  test@oi:properties_each (cb, self);
+  test@oi:properties_each (cb, test);
   test@["abc"float]=3.2;
   test@["times"string]="3.1415";
-  test@oi:properties_each (cb, self);
+  test@oi:properties_each (cb, test);
 
   printf ("%s\n", self@["name"oi]@string:get());
+  printf ("%i arguments\n", args@list:get_size());
 
-  args@list:each ((void*)each_arg_cb, self);
+  args@list:each ((void*)each_arg_cb, NULL);
 
   test@ref:dec();
   return 0;
